79ea3ac75c4ab5c765839fdc38ed34e1585b6453
道化師 <wraith13@hotmail.co.jp>
2020-05-26 11:52:27 +0900

    🛠 実装 ( WIP )

--- source/extension.ts
+++ source/extension.ts
@@ -1,13 +1,33 @@
 import * as vscode from 'vscode';
-interface SettingsEntry
+interface PackageJsonConfigurationProperty
 {
-	id : string ;
-	type : string | string [ ] ;
-	default : any ;
-	minimum ?: number ;
-	maximum ?: number ;
-	overridable ?: boolean ;
-	description ?: string ;
+    type : string | string [ ] ;
+    default : any ;
+    minimum ?: number ;
+    maximum ?: number ;
+    overridable ?: boolean ;
+    description ?: string ;
+}
+interface PackageJsonConfiguration
+{
+    title : string;
+    properties : { [ key : string ] : PackageJsonConfigurationProperty }
+}
+interface PackageJsonContributes
+{
+    configuration : PackageJsonConfiguration
+}
+interface PackageJson
+{
+    name : string ;
+    displayName : string ;
+    description : string ;
+    version : string ;
+    contributes : PackageJsonContributes ;
+} ;
+interface SettingsEntry extends PackageJsonConfigurationProperty
+{
+    id : string ;
 } ;
 const vscodeSettings : SettingsEntry [ ] = [ ];
 export const extentionSettings = ( ): SettingsEntry [ ] =>
@@ -17,91 +37,91 @@ export const extentionSettings = ( ): SettingsEntry [ ] =>
 export const aggregateSettings = ( ): SettingsEntry [ ] => vscodeSettings . concat ( extentionSettings ( ) ) ;
 export const editSettingItem = async ( entry : SettingsEntry ) =>
 (
-	await vscode .window . showQuickPick
-	(
-		[
+    await vscode .window . showQuickPick
+    (
+        [
 
-		] ,
-		{
+        ] ,
+        {
 
-		}
-	)
+        }
+    )
 ) ?. command ( ) ;
 export const editSettings = async (
-	_configurationTarget : vscode . ConfigurationTarget,
-	_overridable : boolean
+    _configurationTarget : vscode . ConfigurationTarget,
+    _overridable : boolean
 ) =>
 (
-	await vscode .window . showQuickPick
-	(
-		( await aggregateSettings ( ) ) . map
-		(
-			i =>
-			({
-				label : i . id ,
-				command : async ( ) => await editSettingItem ( i ),
-			})
-		) ,
-		{
+    await vscode .window . showQuickPick
+    (
+        ( await aggregateSettings ( ) ) . map
+        (
+            i =>
+            ({
+                label : i . id ,
+                command : async ( ) => await editSettingItem ( i ),
+            })
+        ) ,
+        {
 
-		}
-	)
+        }
+    )
 ) ?. command ( ) ;
 export const activate = (context : vscode . ExtensionContext) => context . subscriptions . push
 (
-	vscode . commands . registerCommand
-	(
-		'blitz.editUserSettings',
-		async ( ) => editSettings
-		(
-			vscode . ConfigurationTarget . Global ,
-			false
-		) ,
-	) ,
-	vscode . commands . registerCommand
-	(
-		'blitz.editWorkspaceSettings',
-		async ( ) => editSettings
-		(
-			vscode . ConfigurationTarget . Workspace ,
-			false
-		) ,
-	) ,
-	vscode . commands . registerCommand
-	(
-		'blitz.editFolderSettings',
-		async ( ) => editSettings
-		(
-			vscode . ConfigurationTarget . WorkspaceFolder ,
-			false
-		) ,
-	) ,
-	vscode . commands . registerCommand
-	(
-		'blitz.editUserOverrideSettings',
-		async ( ) => editSettings
-		(
-			vscode . ConfigurationTarget . Global ,
-			true
-		) ,
-	) ,
-	vscode . commands . registerCommand
-	(
-		'blitz.editWorkspaceOverrideSettings',
-		async ( ) => editSettings
-		(
-			vscode . ConfigurationTarget . Workspace ,
-			true
-		) ,
-	) ,
-	vscode . commands . registerCommand
-	(
-		'blitz.editFolderOverrideSettings',
-		async ( ) => editSettings
-		(
-			vscode . ConfigurationTarget . WorkspaceFolder ,
-			true
-		) ,
-	)
+    vscode . commands . registerCommand
+    (
+        'blitz.editUserSettings',
+        async ( ) => editSettings
+        (
+            vscode . ConfigurationTarget . Global ,
+            false
+        ) ,
+    ) ,
+    vscode . commands . registerCommand
+    (
+        'blitz.editWorkspaceSettings',
+        async ( ) => editSettings
+        (
+            vscode . ConfigurationTarget . Workspace ,
+            false
+        ) ,
+    ) ,
+    vscode . commands . registerCommand
+    (
+        'blitz.editFolderSettings',
+        async ( ) => editSettings
+        (
+            vscode . ConfigurationTarget . WorkspaceFolder ,
+            false
+        ) ,
+    ) ,
+    vscode . commands . registerCommand
+    (
+        'blitz.editUserOverrideSettings',
+        async ( ) => editSettings
+        (
+            vscode . ConfigurationTarget . Global ,
+            true
+        ) ,
+    ) ,
+    vscode . commands . registerCommand
+    (
+        'blitz.editWorkspaceOverrideSettings',
+        async ( ) => editSettings
+        (
+            vscode . ConfigurationTarget . Workspace ,
+            true
+        ) ,
+    ) ,
+    vscode . commands . registerCommand
+    (
+        'blitz.editFolderOverrideSettings',
+        async ( ) => editSettings
+        (
+            vscode . ConfigurationTarget . WorkspaceFolder ,
+            true
+        ) ,
+    )
 ) ;
 export const deactivate = ( ) => { } ;
